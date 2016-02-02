eb7aadf359c3237eff392fc8642db2187e0c36cb
scott1024 <1059613472@qq.com>
2016-02-02 19:02:02 +0800

    2. 完成了启动界面的UI

--- app/src/main/java/com/yongf/smartguard/SplashActivity.java
+++ app/src/main/java/com/yongf/smartguard/SplashActivity.java
@@ -1,13 +1,40 @@
 package com.yongf.smartguard;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private TextView tv_splash_version;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_splash_version = (TextView) findViewById(R.id.tv_splash_version);
+        tv_splash_version.setText("版本号：" + getVersionName());
+    }
+
+    /**
+     * 得到应用程序的版本信息
+     * @return 版本
+     */
+    private String getVersionName() {
+        //用来管理手机的APK
+        PackageManager packageManager = getPackageManager();
+        //得到指定APK的功能清单文件
+        try {
+            PackageInfo packageInfo = packageManager.getPackageInfo(getPackageName(), 0);
+            return packageInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        return "";
     }
 }
